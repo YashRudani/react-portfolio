17f6f387169eb4e951b68c9e75a100f58388cf6c
davisk4rpi <davisk4rpi@gmail.com>
2017-07-24 15:20:05 -0700

    playing with css

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
+        <header className="App-header">
+          <span className="name-header">Kieran Davis</span>
+        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
