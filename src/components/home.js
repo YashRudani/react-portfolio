f70fd296fb920e9714dc2fdb0e8cb9c71b4cff95
davisk4rpi <davisk4rpi@gmail.com>
2017-07-25 17:31:54 -0700

    format skill section

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,27 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return (
-      <section>
-        <h1>Welcome!</h1>
+      <section id="home-page">
+        <div className="center-align">
+          <h1>Kieran Davis</h1>
+          <div className="flow-text">
+            <div className="hide-on-med-and-down">
+              Product Management, Front-End, & Full-Stack Development
+            </div>
+            <div className="hide-on-large-only">
+              Product Management
+            </div>
+            <div className="hide-on-large-only">
+              Front-End Development
+            </div>
+            <div className="hide-on-large-only">
+              Full-Stack Development
+            </div>
+            <div>
+              React | Rails | Sass | JS | Ruby | Python
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
